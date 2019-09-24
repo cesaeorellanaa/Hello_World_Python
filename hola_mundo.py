0095560040c7f7886d8d5c756c6a04fd3751009d
César Orellana <ce.orellanaa@alumnos.duoc.cl>
2019-09-24 10:16:24 -0300

    agregamos el hola mundo

---
+++ hola_mundo.py
@@ -0,0 +1,6 @@
+#!usr/bin/python3
+
+# la linea de arriba es solo para correr en linux, en windows no es necesario.
+#guardar en la carpeta del proyecto con el nombre "'hola_mundo.py"
+
+print('Hola Mundo')
